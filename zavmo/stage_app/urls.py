2e62a6884dc26bc369bd295cba70172e180c3b53
Smrithi Shaji <smrithi.1899@gmail.com>
2024-10-09 22:37:32 +0530

    Added ChatAPI with session initiation, stage handling, and JWT-based authentication

--- zavmo/stage_app/urls.py
+++ zavmo/stage_app/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import create_org, create_user, UserProfileView
+from .views import create_org, create_user, UserProfileView, ChatAPI
 
 urlpatterns = [
     path('api/create_org/',create_org, name='create_org'),
     path('api/create_user/',create_user,name='create_user'),
     path('api/user/profile/',UserProfileView.as_view(), name='user-profile'),
+    path('api/zavmo/chat/', ChatAPI.as_view(), name='chat-api'),
 ]
