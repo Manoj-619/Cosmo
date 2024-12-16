1b4d0fc4ada0424e102481210e5389b28dd400b0
Mumtaz <mumtazrahmani10@gmail.com>
2024-12-16 15:56:13 +0530

    increase competency field - max_length

--- zavmo/helpers/agents/profile.py
+++ zavmo/helpers/agents/profile.py
@@ -84,13 +84,18 @@ class transfer_to_tna_assessment_stage(StrictTool):
         summary = profile.get_summary()
         if profile.current_role:
             all_competencies = get_nos_competencies_with_criteria(profile.current_role)
-            for item in all_competencies['nos']:
-                TNAassessment.objects.create(
-                    user=profile.user,
-                    sequence_id=context['sequence_id'],
-                    competency=item['competency'],
-                    blooms_taxonomy_criteria=item['blooms_taxonomy_criteria']
-                )
+            try:
+                for item in all_competencies['nos']:
+                    competency = item['competency']
+                    TNAassessment.objects.create(
+                        user=profile.user,
+                        sequence_id=context['sequence_id'],
+                        competency=competency,
+                        blooms_taxonomy_criteria=item['blooms_taxonomy_criteria']
+                    )
+            except Exception as e:
+                logging.error(f"Error creating TNA assessments: {e}, Competency: {competency}")
+
             logging.info(f"TNA assessments created: {TNAassessment.objects.filter(user=profile.user, sequence_id=context['sequence_id']).count()}")
         agent = tna_assessment_agent
         agent.start_message = f"Here is the learner's profile: {summary}"
