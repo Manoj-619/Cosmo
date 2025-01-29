329d2a2a9d09f0c6443e0e977acbabade25f00a7
amithvn <amithesh.vn@kenpath.io>
2025-01-29 11:01:28 +0530

    Chat xAPI

--- zavmo/stage_app/tasks.py
+++ zavmo/stage_app/tasks.py
@@ -8,53 +8,6 @@ from .api_config import API_CONFIG
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-# def build_and_execute_api(api_key, context):
-#     """Build and execute the API call for a given key."""
-#     api_details = API_CONFIG[api_key]
-#     payload = api_details["payload_builder"](context)
-#     url = api_details["url"]
-#     headers = {"Content-Type": "application/json"}
-    
-#     # Execute the API call
-#     response = requests.post(url, headers=headers, data=json.dumps(payload))
-    
-#     # Handle response
-#     if response.status_code == 200:
-#         logger.info(f"{api_key} Success:", response.json())
-#     else:
-#         logger.error(f"{api_key} Failed with status code {response.status_code}: {response.text}")
-
-# # Keep track of which APIs have been called
-# called_apis = set()
-
-# @shared_task(name="xAPI_celery_task")
-# def xAPI_celery_task(context):
-#     """Main function to call APIs based on conditions."""
-    
-#     # Define API groups based on conditions
-#     api_groups = {
-#         "discover": ["education_api", "learning_goals_api"],  
-#         "discuss": ["learning_style_api", "learning_schedule_api", "curriculum_registration_api"], 
-#     }
-    
-# # Check and call APIs based on context
-#     for key, api_list in api_groups.items():
-#         if context.get(key):  # Check if the key (e.g., discover or discuss) is not empty
-#             logger.info(f"Context key '{key}' is not empty. Calling associated APIs...")
-#             for api_key in api_list:
-#                 # Check if the API has already been called
-#                 if api_key not in called_apis:
-#                     try:
-#                         logger.info(f"Calling {api_key}...")
-#                         build_and_execute_api(api_key, context)
-#                         logger.info(f"Finished calling {api_key}.")
-#                         # Mark this API as called
-#                         called_apis.add(api_key)
-#                     except Exception as e:
-#                         logger.error(f"Error calling {api_key}: {e}")
-
-#     return "xAPI trigger Done"
-
 @shared_task(name="xAPI_chat_celery_task")
 def xAPI_chat_celery_task(latest_user_message, latest_stage,email):
 
@@ -74,6 +27,23 @@ def xAPI_chat_celery_task(latest_user_message, latest_stage,email):
         }
     }
 
+    response = requests.post(url, headers=headers, json=data)
+    logger.info(response.text)
+    return response.json()
+
+@shared_task(name="xAPI_stage_celery_task")
+def xAPI_stage_celery_task(stage_data,email,name):
+    url = 'https://learninglocker.zavmo.ai/v1/statements/stage'
+    headers = {
+        'Content-Type': 'application/json'
+    }
+    data = {
+        "actor": {
+            "name": name,
+            "email": email
+        },
+        "stage": stage_data
+    }
     response = requests.post(url, headers=headers, json=data)
     logger.info(response.text)
     return response.json()
