14b729c848283bd6786448b4841a3dd68c1d8ccb
addypy <aditya0chhabra@gmail.com>
2025-03-26 12:46:04 +0530

    Creating new agent structure. Plan to replace helpers.agents with agents.

---
+++ zavmo/agents/utils.py
@@ -0,0 +1,60 @@
+
+import os
+import codecs
+import yaml
+from helpers.chat import get_prompt
+
+def get_yaml_data(yaml_path, yaml_dir="assets/data"):
+    """Load a YAML file containing field data.
+
+    Args:
+        yaml_path (str): Path to the YAML file.
+        yaml_dir (str, optional): Directory containing the YAML file. Defaults to 'assets/data'.
+
+    Returns:
+        dict: A dictionary of field data.
+
+    Raises:
+        UnicodeDecodeError: If there's an encoding issue with the file.
+        yaml.YAMLError: If there's an issue parsing the YAML content.
+        FileNotFoundError: If the specified file doesn't exist.
+    """
+    # Check if yaml_path ends with .yaml or .yml
+    if not yaml_path.endswith(('.yaml', '.yml')):
+        yaml_path += '.yaml'
+    yaml_path = os.path.join(yaml_dir, yaml_path)
+    
+    try:
+        with codecs.open(yaml_path, 'r', encoding='utf-8') as f:
+            return yaml.safe_load(f)
+    except UnicodeDecodeError as e:
+        print(f"Encoding error in file {yaml_path}: {e}")
+        print("Make sure the file is saved with UTF-8 encoding.")
+        raise
+    except yaml.YAMLError as e:
+        print(f"YAML parsing error in file {yaml_path}: {e}")
+        raise
+    except FileNotFoundError:
+        print(f"File not found: {yaml_path}")
+        raise
+
+
+
+def get_agent_instructions(stage_name: str) -> str:
+    """
+    Compile instructions for the agent.
+    
+    Args:
+        stage_name (str): The name of the current stage.
+
+    Returns:
+        str: Instructions for the agent.
+    """
+    conf_data       = get_yaml_data(stage_name.lower())
+    agent_keys      = ['name', 'description', 'instructions', 'examples', 'completion_condition', 'next_stage', 'next_stage_description']
+    prompt_context  = {k:v for k,v in conf_data.items() if k in agent_keys}
+
+    system_content  = get_prompt('probe.md', 
+                                    context=prompt_context,
+                                    prompt_dir="assets/prompts")
+    return system_content   
